0d890b71398971974b6275205cc308f049b5ceec
Paul Calzado <pafcalzado@gmail.com>
2024-01-15 13:26:11 +0800

    Create router and server

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/skullzado/blog-aggregator
 go 1.21.5
 
 require (
-	github.com/go-chi/chi/v5 v5.0.11 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load(".env")
 	port := os.Getenv("PORT")
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{}))
+
+	subRouter := chi.NewRouter()
+
+	router.Mount("/v1", subRouter)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	log.Printf("Serving on port: %s", port)
+	log.Fatal(srv.ListenAndServe())
 }
