5bbdceb80e2aa5979f799c4a83bb616a41fd3065
Paul Calzado <pafcalzado@gmail.com>
2024-01-15 14:03:57 +0800

    Update files to copy guided project's solution

--- error.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "net/http"
-
-func errorHandler(w http.ResponseWriter, r *http.Request) {
-	respondWithError(w, 500, "Internal Server Error")
-}
--- main.go
+++ main.go
@@ -12,22 +12,33 @@ import (
 
 func main() {
 	godotenv.Load(".env")
+
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
 
 	router := chi.NewRouter()
-	router.Use(cors.Handler(cors.Options{}))
 
-	subRouter := chi.NewRouter()
-	subRouter.Get("/readiness", handlerReadiness)
-	subRouter.Get("/err", errorHandler)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
-	router.Mount("/v1", subRouter)
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
 
+	router.Mount("/v1", v1Router)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
 	}
 
-	log.Printf("Serving on port: %s", port)
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
