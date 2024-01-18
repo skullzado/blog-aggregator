b46320a3ec0fb1a469591de9e54e0f8bf8df4e90
Paul Calzado <pafcalzado@gmail.com>
2024-01-18 20:23:15 +0800

    Add feeds table

---
+++ sql/schema/003_feeds.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE feeds (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    user_id UUID NOT NULL,
+    CONSTRAINT FK_users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+-- +goose Down
+DROP TABLE feeds;
